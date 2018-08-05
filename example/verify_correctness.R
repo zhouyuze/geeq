c02c31ad573821d4c549723724651a54a6ed3c53
Yuze Zhou <ruczyz@126.com>
2018-08-05 10:02:49 +0800

    update example

--- example/verify_correctness.R
+++ example/verify_correctness.R
@@ -6,8 +6,8 @@ fit.ohio.geepack <- geese(resp ~ age + smoke + age:smoke, id=id, data=ohio,
                           family=binomial, corstr="exch")
 fit.ohio.geem <- geem(resp ~ age + smoke + age:smoke, id=id, data=ohio,
                       family=binomial, corstr="exchangeable")
-fit.ohio.geeq <- gee(resp ~ age + smoke + age:smoke, id=id, data=ohio,
-                     family=binomial(), corstr="exchangeable")
+fit.ohio.geeq <- geeq(resp ~ age + smoke + age:smoke, id=id, data=ohio,
+                     method='gee', family=binomial(), corstr="exchangeable")
 
 data(dietox)
 dietox$Cu <- as.factor(dietox$Cu)
@@ -15,7 +15,7 @@ fit.dietox.geepack <- geeglm(Weight ~ Time + Cu + Cu * Time, id=Pig, data = diet
                               family=gaussian,corstr="exch")
 fit.dietox.geem <- geem(Weight ~ Time + Cu + Cu * Time, id=Pig, data = dietox,
                         family=gaussian,corstr="exchangeable")
-fit.dietox.geeq <- gee(Weight ~ Time + Cu + Cu * Time, id=Pig, data = dietox,
-                       family=gaussian,corstr="exchangeable")
+fit.dietox.geeq <- geeq(Weight ~ Time + Cu + Cu * Time, id=Pig, data = dietox,
+                       method='gee', family=gaussian,corstr="exchangeable")
 
 
